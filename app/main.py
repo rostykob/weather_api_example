b92db16e828ea30500472d5f33c6fb3df0683ef2
Rostyslav K <r.kobylianskyi@gmail.com>
2025-01-15 16:36:43 +0100

    fix main

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from .api import weather
+from .config.settings import get_settings
+
+app = FastAPI(
+    title=get_settings().PROJECT_NAME,
+    debug=get_settings().DEBUG,
+    openapi_url=f"{get_settings().API_V1_PREFIX}/openapi.json",
+    docs_url=f"{get_settings().API_V1_PREFIX}/docs",
+)
+
+
+app.router.include_router(weather.router)
